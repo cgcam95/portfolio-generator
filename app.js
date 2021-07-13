1690bfc66906b33d1581b37793fde55d91f6fe57
Camil Grabowski <camil.grabowski@uconn.edu>
2021-07-13 17:47:49 -0400

    bugged

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
+// const generatePage = () => 'Name: Camil, Github: camilhub';
+
+const generatePage = (userName, githubName) => {
+    return `
+    Name: ${userName}
+    GitHub: ${githubName}
+    `;
+    
+}
+console.log(generatePage(name, github));
 
 
-const printProfileData = profileDataArr => {
-    //This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('==============');
 
 
-    //Is the same as this...
-    profileDataArr.forEach(profileItem => 
-        console.log(profileItem));
 
-};
 
-printProfileData(profileDataArgs)
+
+
+
+
+
+
+
+
+
+
+
+
+// const printProfileData = profileDataArr => {
+//     //This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('==============');
+
+
+//     //Is the same as this...
+//     profileDataArr.forEach(profileItem => 
+//         console.log(profileItem));
+
+// }; hi;`
+
+// printProfileData(profileDataArgs)
