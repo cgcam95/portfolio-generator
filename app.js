fafd2300e0137123c3499a0aa4b1cba8c2da8a35
Camil Grabowski <camil.grabowski@uconn.edu>
2021-07-16 16:03:17 -0400

    questions set

--- app.js
+++ app.js
@@ -1,15 +1,128 @@
 const inquirer = require('inquirer');
 
-inquirer
-.prompt([
+const promptUser = () => {
+    return inquirer.prompt([
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your Github Username',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your Github Username');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
     }
-])
+]);
+};
 
-.then(answers => console.log(answers));
+const promptProject = portfolioData => {
+    console.log(`
+    =================
+    Add a New project
+    =================
+    `);
+
+//If theres no 'projects' array property, create one
+if (!portfolioData.projects) {
+    portfolioData.projects = []; // I DONT UNDERSTAND THIS!!!!
+}
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your projects name!');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a description of your project');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true
+                } else {
+                    console.log('Please enter your Github link');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData); // how does it know to just repeat the questions about the project?
+        } else {
+            return portfolioData;
+        }
+    }) 
+};
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+});
+
 
 
 
