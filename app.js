af4525707d406ace3ff18ca028c8b493730b5744
Camil Grabowski <camil.grabowski@uconn.edu>
2021-07-19 10:12:50 -0400

    finished site

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// const fs = require('fs');
-fs.writeFile('./dist/index.html');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
 
@@ -134,26 +133,42 @@ if (!portfolioData.projects) {
     }) 
 };
 
-promptUser()
-.then(promptProject)
-.then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
+// promptUser()
+// .then(promptProject)
+// .then(portfolioData => {
+//     const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./dist/index.html', pageHTML, err => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log('Page created! Check out index.html in this directory to see it!');
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log('Page created! Check out index.html in this directory to see it!');
       
-        fs.copyFile('./src/style.css', './dist/style.css', err => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-          console.log('Style sheet copied successfully!');
-        });
-    });
-});
-
-
+//         fs.copyFile('./src/style.css', './dist/style.css', err => {
+//           if (err) {
+//             console.log(err);
+//             return;
+//           }
+//           console.log('Style sheet copied successfully!');
+//         });
+//     });
+// });
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
